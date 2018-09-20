77c4d1e392ba22ba696cba94121e1260e0b2acbe
ReshanMaduka <reshanmaduka615@gmail.com>
2018-09-20 11:47:37 +0530

    springmvc+hibernate+jsp

---
+++ spring5-mvc-hibernate-example/src/main/java/com/howtodoinjava/demo/spring/main/WebAppConfig.java
@@ -0,0 +1,60 @@
+package com.howtodoinjava.demo.spring.main;
+
+import com.howtodoinjava.demo.spring.repository.CustomerRepository;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Created by Reshan on 14/08/2018.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.howtodoinjava.demo.spring")
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackageClasses = {CustomerRepository.class})
+public class WebAppConfig implements WebMvcConfigurer {
+
+
+    @Bean
+    public InternalResourceViewResolver resolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        return source;
+    }
+
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+        validator.setValidationMessageSource(messageSource());
+        return validator;
+    }
+
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+}
